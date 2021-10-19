da1def0b6a0d560a0a5cb9856f73103005a82239
Jeremiah <jeremiahtaylor2017@gmail.com>
2021-10-19 01:50:16 -0700

    Added Menu and functionally decomposed project

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -1,25 +1,11 @@
 package cinema;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
 
     public static void main(String[] args) {
-//        String cinema = "Cinema:";
-//        System.out.println(cinema);
-//
-//        for (int i = 1; i < 9; i++) {
-//            System.out.print(" " + i);
-//        }
-//        System.out.println();
-//        for (int j = 1; j < 8; j++) {
-//            System.out.print(j + " ");
-//            for (int k = 0; k < 8; k++) {
-//                System.out.print("S ");
-//            }
-//            System.out.println();
-//        }
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the number of rows:");
@@ -27,20 +13,78 @@ public class Cinema {
         System.out.println("Enter the number of seats in each row:");
         int seats = scanner.nextInt();
 
+        char[][] cinema = new char[rows][seats];
+        for (char[] chars : cinema) {
+            Arrays.fill(chars, 'S');
+        }
+
+        while (true) {
+            showMenu();
+            int menuOption = scanner.nextInt();
+            switch (menuOption) {
+                case 0:
+                    return;
+                case 1:
+                    showSeats(rows, seats, cinema);
+                    break;
+                case 2:
+                    buyTicket(rows, seats, cinema);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    public static void showMenu() {
+        System.out.println();
+        System.out.println("1. Show the seats");
+        System.out.println("2. Buy a ticket");
+        System.out.println("0. Exit");
+    }
+
+
+    public static void showSeats(int rows, int seats, char[][] s) {
+        System.out.println();
+        System.out.println("Cinema:");
+        for (int i = 1; i <= seats; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 0; i < rows; i++) {
+            System.out.print(i + 1);
+            for (int j = 0; j < seats; j++) {
+                System.out.print(" " + s[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void buyTicket(int rows, int seats, char[][] s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Enter a row number:");
+        int selectedRow = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int selectedSeat = scanner.nextInt();
+
         int totalSeats = rows * seats;
         int price = 10;
-        int totalPrice;
+        int selectedPrice;
         if (totalSeats <= 60) {
-            totalPrice = price * totalSeats;
+            selectedPrice = price;
         } else {
             int front = rows / 2;
             int backPrice = 8;
-            int frontTotal = front * price * seats;
-            int backTotal = (rows - front) * backPrice * seats;
-            totalPrice = frontTotal + backTotal;
+            if (selectedRow <= front) {
+                selectedPrice = price;
+            } else {
+                selectedPrice = backPrice;
+            }
         }
-
-        System.out.println("Total income:");
-        System.out.print("$" + totalPrice);
+        s[selectedRow - 1][selectedSeat - 1] = 'B';
+        System.out.println("Ticket price: $" + selectedPrice);
     }
 }
